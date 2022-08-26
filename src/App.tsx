2768b04efa1c80df7b42ffe7261fd5a69ef6b00a
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-08-27 01:47:40 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="flex h-screen flex-col items-center justify-center space-y-2">
+        <h1 className="text-5xl font-medium">Hello World</h1>
+        <p className="text-2xl">This is where the magic happens!</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
